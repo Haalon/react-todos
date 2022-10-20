f19e9662b3acc8e2bf78a64ec691b2649f1800d8
Haalon <haalon@yandex.com>
2022-10-20 17:17:47 +0300

    Remove state from App

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { TodoList } from "./components/todoList";
 import "./styles.scss";
 
 export default function App() {
-  const [todos, setTodos] = useState([
+  const todos = [
     { text: "Buy milk", done: true },
     { text: "Buy bread", done: false }
-  ]);
+  ];
 
   return (
     <div className="todoListApp">
