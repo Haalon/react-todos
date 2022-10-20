03f95d2751e32ff35a378e0a48c3094996900863
Haalon <haalon@yandex.com>
2022-10-20 16:12:28 +0300

    Add todo creation

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent, useCallback, useState } from "react";
 import classes from "./todoList.module.scss"
 
 type TodoListItem = {
@@ -10,16 +10,33 @@ type TodoListProps = {
   todos: TodoListItem[];
 };
 
-export const TodoList = ({ todos }: TodoListProps) => {
+export const TodoList = ({ todos: initialTodods }: TodoListProps) => {
+  const [todos, setTodos] = useState(initialTodods);
+  const [newTodoText, setNewTodoText] = useState("");
+
+  const handleChange = useCallback((e:React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodoText(e.target.value)
+  }, [])
+
+  const addTodo = useCallback((e:React.MouseEvent<HTMLButtonElement>) => {
+    setTodos([{text: newTodoText, done: false}, ...todos])
+  }, [newTodoText])
 
 
   return (
-    <ul className={classes.todoList}>
-      {todos.map((item, i) => (
-        <li key={i}>
-          <span data-testid={`todo${i}`}>{item.text}</span>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className={classes.addTodo}>
+        <input onChange={handleChange}></input>
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul className={classes.todoList}>
+        {todos.map((item, i) => (
+          <li key={i}>
+            <span data-testid={`todo${i}`}>{item.text}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+
   );
 };
